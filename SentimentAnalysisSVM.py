4f26bea852310910d29e4092705962c1f08432a5
SeifEldin15 <144861107+SeifEldin15@users.noreply.github.com>
2024-05-04 17:07:38 +0300

    Update SentimentAnalysisSVM.py
    changed dataset, different splitting method.

--- SentimentAnalysisSVM.py
+++ SentimentAnalysisSVM.py
@@ -1,40 +1,40 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report
 from sklearn import svm
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
-# Load the text file
-file_path = input("Enter the path to the text file: ")
-with open(file_path, 'r') as file:
-    text = file.read()
 
-# Create a DataFrame with the text
-data = {'Content': [text]}
-input_data = pd.DataFrame(data)
+file_path = "data/movie.csv"
+data = pd.read_csv(file_path)
 
-# Load the training data and test data
-trainData = pd.read_csv("data/train.csv")
-testData = pd.read_csv("data/test.csv")
 
-# Create feature vectors
+trainData, testData = train_test_split(data, test_size=0.2, random_state=42)
+
 vectorizer = TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True)
-train_vectors = vectorizer.fit_transform(trainData['Content'])
-test_vectors = vectorizer.transform(testData['Content'])
-input_vectors = vectorizer.transform(input_data['Content'])
+train_vectors = vectorizer.fit_transform(trainData['text'])
+test_vectors = vectorizer.transform(testData['text'])
 
-# Perform classification with SVM, kernel=linear
 classifier_linear = svm.SVC(kernel='linear')
-classifier_linear.fit(train_vectors, trainData['Label'])
+classifier_linear.fit(train_vectors, trainData['label'])
+
+input_file_path = "test_file.txt" 
+with open(input_file_path, 'r') as file:
+    input_text = file.read().splitlines()
+
+input_data = {'text': input_text}
+input_df = pd.DataFrame(input_data)
+input_vectors = vectorizer.transform(input_df['text'])
+
 prediction_linear = classifier_linear.predict(input_vectors)
 
-if prediction_linear[0] == 'pos':
+if prediction_linear[0] == 1:
     print("Sentiment: Positive")
-elif prediction_linear[0] == 'neg':
+elif prediction_linear[0] == 0:
     print("Sentiment: Negative")
-else:
-    print("Sentiment: Neutral")
 
-# Generate classification report for test data
+
+
 test_predictions = classifier_linear.predict(test_vectors)
 print("\nClassification Report for Test Data:")
-print(classification_report(testData['Label'], test_predictions))
+print(classification_report(testData['label'], test_predictions))
